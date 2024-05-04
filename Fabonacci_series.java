36755950613c1830cc868569b7638cb9e933044f
Amit Kumar Tiwari <160128473+amittiwari05@users.noreply.github.com>
2024-05-04 13:07:21 +0530

    main

---
+++ Fabonacci_series.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * Fabonacci_series
+ */
+public class Fabonacci_series {
+
+    public static void main (String [] args)
+    {
+        Scanner sc = new Scanner (System.in);
+        int x=0, y=1, z=0, n;
+        System.out.println("Enter maximum value:");
+        n = sc.nextInt();
+        while (z<=n)
+        {
+            System.out.println(z);
+            x = y;
+            y = z;
+            z = x+y;
+        }
+    }
+}
