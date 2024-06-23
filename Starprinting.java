91e33c860b8134e0e2bae9742622343f6728c521
Amit Kumar Tiwari <160128473+amittiwari05@users.noreply.github.com>
2024-06-23 23:08:54 +0530

    main

---
+++ Starprinting.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Starprinting
+ */
+public class Starprinting {
+
+    public static void main(String args [])
+    {
+        int n,i,j;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter number of rows:");
+        n= sc.nextInt();
+        for (i=1;i<=n;i++)
+        {
+            for (j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
